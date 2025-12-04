752ecded46ef479e6ae14b623e6bd3a9d0dfae46
adars <adarshsen9115@gmail.com>
2025-12-04 19:45:20 +0530

    Added Shader abstraction specific to openGL to help with the glMath.h

---
+++ Shader.cpp
@@ -0,0 +1,71 @@
+#include "Shader.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include"glMath.h"
+
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath) {
+	std::ifstream vFile(vertexPath);
+	std::ifstream fFile(fragmentPath);
+	std::stringstream vStream, fStream;
+
+	vStream << vFile.rdbuf();
+	fStream << fFile.rdbuf();
+
+	std::string vCode = vStream.str();
+	std::string fCode = fStream.str();
+
+	const char* vShaderCode = vCode.c_str();
+	const char* fShaderCode = fCode.c_str();
+
+	unsigned int vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vShaderCode, NULL);
+	glCompileShader(vertex);
+
+	unsigned int fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fShaderCode, NULL);
+	glCompileShader(fragment);
+
+	ID = glCreateProgram();
+	glAttachShader(ID, vertex);
+	glAttachShader(ID, fragment);
+	glLinkProgram(ID);
+
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+}
+
+Shader::~Shader() {
+	glDeleteProgram(ID);
+}
+
+void Shader::use() const {
+	glUseProgram(ID);
+}
+
+
+
+void Shader::setFloat(const std::string& name, float value) const {
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setVec2(const std::string& name, const glm::vec2& value) const {
+	glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+
+void Shader::setMat4(const std::string& name, const mat4& mat) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, mat.ptr());
+}
+
+
+void Shader::setVec3(const std::string& name, const vec3& value) const
+{
+	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, value.ptr());
+}
+
+void Shader::setInt(const std::string& name, int value) const {
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
---
+++ Shader.h
@@ -0,0 +1,27 @@
+#ifndef SHADER_CLASS_H
+#define SHADER_CLASS_H
+
+
+
+
+#include <string>
+#include <glad/glad.h>
+#include"glMath.h"
+
+class Shader {
+public:
+	unsigned int ID;
+
+	Shader(const char* vertexPath, const char* fragmentPath);
+	~Shader();
+	void use() const;
+
+	void setFloat(const std::string& name, float value) const;
+	void setVec2(const std::string& name, const glm::vec2& value) const;
+	void setMat4(const std::string& name, const mat4& mat) const;
+	void setVec3(const std::string& name, const vec3& value) const;
+	void setInt(const std::string& name, int value) const;
+};
+
+
+#endif 
