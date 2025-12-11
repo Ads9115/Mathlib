a37a31533b19459ae30cd315a9cc11833ac6c9d8
adars <adarshsen9115@gmail.com>
2025-12-12 00:45:50 +0530

    added shader Abstraction

--- Shader.cpp
+++ Shader.cpp
@@ -66,6 +66,11 @@ void Shader::setVec3(const std::string& name, const vec3& value) const
 	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, value.ptr());
 }
 
+void Shader::setVec4(const std::string& name, const vec4& value) const
+{
+	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, value.ptr());
+}
+
 void Shader::setInt(const std::string& name, int value) const {
 	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
